f1f29252cee5abc4c7f262d0704d25af23bcf513
Heitor Tashiro Sergent <heitortsergent@gmail.com>
2015-03-30 16:25:39 -0500

    Update index.jade layout
    Add Evernote Logo and favicon

--- sample/express/app.js
+++ sample/express/app.js
@@ -11,7 +11,7 @@ app.configure(function(){
   app.set('port', process.env.HTTP_PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
+  app.use(express.favicon(path.join(__dirname, 'public','images','favicon.ico'))); 
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
