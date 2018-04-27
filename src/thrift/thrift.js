82202af2c6a9f349a308363891fc74e705661296
Marie <maclemes@microsoft.com>
2018-04-27 15:35:07 -0400

    Catch readMessageBegin failures

--- src/thrift/thrift.js
+++ src/thrift/thrift.js
@@ -182,7 +182,16 @@ Thrift.Method.prototype.processResponse = function (response, callback) {
 
     callback = callback || Thrift.Method.noop;
 
-    var header = response.readMessageBegin();
+    try {
+      header = response.readMessageBegin();
+    }
+    catch (exception) {
+      err = Error('Failed to readMessageBegin. Received [' + exception + ']');
+      response.readMessageEnd();
+      callback(err);
+      return;
+    }
+
     if (header.mtype == Thrift.MessageType.EXCEPTION) {
         err = Thrift.TApplicationException.read(response);
         response.readMessageEnd();
